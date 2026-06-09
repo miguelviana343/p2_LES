c3c35d23ef8a85e014d2442f1cf1953c35e9ff68
Miguel <miguel.viana343@gmail.com>
2026-06-09 13:33:21 -0300

    feat: index.js, app.js e style especialidades e sobre

---
+++ p2les/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './views/Home';
+import Sobre from './views/Sobre';
+import Especialidades from './views/Especialidades';
+import Medicos from './views/Medicos';
+import Agendamento from './views/Agendamento';
+import Contato from './views/Contato';
+import './styles/global.css';
+
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/especialidades" element={<Especialidades />} />
+      <Route path="/medicos" element={<Medicos />} />
+      <Route path="/agendamento" element={<Agendamento />} />
+      <Route path="/contato" element={<Contato />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+);
+
+export default App;
